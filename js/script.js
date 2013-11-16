1bfa3cb12f2f59c451958b7a7507ab55e3e892f1
Spencer Carli <spencercarli@gmail.com>
2013-11-16 18:42:04 -0500

    Random background color

---
+++ js/script.js
@@ -0,0 +1,4 @@
+//Enter list of bgcolors:
+var bgcolorlist=new Array("#DFDFFF", "#FFFFBF", "#80FF80", "#EAEAFF", "#C9FFA8", "#F7F7F7", "#FFFFFF", "#DDDD00")
+
+document.body.style.background=bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]
